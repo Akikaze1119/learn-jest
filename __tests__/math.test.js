16d27b977373b63d2deee4c62c69b711475fec5c
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2025-07-11 21:59:40 -0700

    fix: add test using function

--- __tests__/math.test.js
+++ __tests__/math.test.js
@@ -3,3 +3,7 @@ const sum = require('../src/math');
 test('adds 1 + 2 to be 3', () => {
   expect(1 + 2).toBe(3);
 });
+
+test('adds 1 + 2 to be 3 with sum function', () => {
+  expect(sum(1, 2)).toBe(3);
+});
